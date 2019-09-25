8c5fa8bcd375200d2cc193b3165e9dfdcffef98c
Jaanus Padrik <jaanus.padrik@hendrikson.ee>
2019-09-25 19:36:00 +0300

    25.09.2019

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
+import Chips from "./Chips";
+import Sardines from "./Sardines";
+import Soda from "./Soda";
+import VendingMachine from "./VendingMachine";
+import {Route} from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>HHHHH</h1>
-        <p>LLLLLLL</p>
+      <div className="App">
+        <Route exact path='/' render={() => <VendingMachine />} />
+        /*<VendingMachine />
+        <Chips />
+        <Sardines />
+        <Soda />*/
       </div>
     );
   }
